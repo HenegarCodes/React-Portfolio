e668d6a2e029ea1db14868954648bb76a46ada5a
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-01-22 12:19:04 -0700

    adding navbar. fully functional and not fuly complete hamburger menu needs to be adjust left and size slightly up. on to the homer section.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Background from "./components/background.jsx";
-
+import Navbar from "./components/Navbar.jsx";
 
 export const App = () => {
   return (
     <div className="App">
       <Background />
+      <div><Navbar /></div>
     </div>
+    
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,41 @@
-import React, { Component } from "react";
-class Navbar extends Component{
-    render() {
-        return (
-            
-        )
-    }
-}
+import React, { useState } from "react";
+import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faBriefcase, faUser, faAddressCard, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
+const Navbar = () => {
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        console.log("Toggle Menu clicked");
+        setMenuOpen(!isMenuOpen);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
+
+    return (
+        <div className="Navbar">
+            <div className={`hamburger ${isMenuOpen ? "menu-open" : ""}`} onClick={toggleMenu}>
+                ☰
+            </div>
+            {isMenuOpen && (
+                <div className="menu-overlay">
+                    <ul className="menu menu-open">
+                        <li><a href="#"><FontAwesomeIcon icon={faHouse} /></a></li>
+                        <li><a href="#"><FontAwesomeIcon icon={faBriefcase} /></a></li>
+                        <li><a href="#"><FontAwesomeIcon icon={faUser} /></a></li>
+                        <li><a href="#"><FontAwesomeIcon icon={faAddressCard} /></a></li>
+                    </ul>
+                    <div className="close-button" onClick={closeMenu}>
+                    <FontAwesomeIcon icon={faTimes} />
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
