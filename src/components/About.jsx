34cf53b40329ee6b66dc75928fe7cc4dc4a8966e
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-01-23 21:58:36 -0700

    added more styling adn working on the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,11 @@
 import React from 'react'
 import './about.css'
 
-
 const About = () => {
     return (
-        <div className="container">
-        <div className="about-section">
-          <h2>About Me</h2>
-          <img src="your-profile-picture.jpg" alt="Profile Picture" className="profile-picture" />
-        </div>
-        <div className="content-section">
-          <p>
-            Hello! 👋 I'm Spencer Henegar, a passionate and motivated software developer on the verge of completing an intensive bootcamp journey. Set to receive my certificate in February, I've delved into the dynamic world of full-stack engineering.
-          </p>
-          <p>
-            <strong>Skills Snapshot:</strong><br />
-            - Frontend: Proficient in crafting engaging user interfaces using HTML, CSS, and JavaScript.<br />
-            - Frameworks: Experienced in leveraging the power of React.js for building dynamic and responsive web applications.<br />
-            - Backend: Well-versed in server-side development with Express.js, connecting the dots and ensuring seamless functionality.<br />
-            - Databases: Comfortable working with both NoSQL databases for flexibility and MySQL for robust relational data management.<br />
-            - Deployment & Version Control: Experienced in deploying applications on platforms like Heroku and managing version control with Git/GitHub.
-          </p>
-          <p>
-            I thrive on challenges, and my journey has equipped me with the versatility to tackle a spectrum of technologies and tools. Whether it's front-end aesthetics or back-end functionality, I find joy in creating robust and user-friendly applications.
-          </p>
-          <p>
-            <strong>Let's Code Together! 🚀</strong>
-          </p>
+      <div className="container">
+        <div className="left-section">
+          <h2>About</h2>
         </div>
       </div>
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,7 +8,8 @@ const Projects = () => {
       <div className="project-card">
         <img src="/BudgetBuddy.jpg" alt="Project 1" />
         <h2>Budget Buddy</h2>
-        <p>Budgte Buddy allows users to track their finances and goals strictly to allow for accurate track and goal attaining.</p>
+        <p>
+Budget Buddy empowers users to seamlessly track their finances and pursue their goals with precision. Unleash the power of accurate tracking for achieving your financial milestones.</p>
         <div className="project-buttons">
           <a href="https://github.com/HenegarCodes/budget-tracker-team-5" className="github-button">GitHub</a>
           <a href="https://app-budget-buddy-17170e451f60.herokuapp.com/" className="deployed-button">Deployed</a>
@@ -18,7 +19,8 @@ const Projects = () => {
       <div className="project-card">
         <img src="/PasswordGenerator.jpg" alt="Project 2" />
         <h2>Password Generator</h2>
-        <p>Password Generator allows user sot creat unique and secure passwords instantly with mutliple options of customization. </p>
+        <p>
+Password Generator enables users to quickly create unique and secure passwords with various customization options, ensuring a straightforward and secure password creation process. </p>
         <div className="project-buttons">
           <a href="https://github.com/HenegarCodes/passwordGenerator" className="github-button">GitHub</a>
           <a href="https://henegarcodes.github.io/passwordGenerator/" className="deployed-button">Deployed</a>
@@ -28,7 +30,8 @@ const Projects = () => {
       <div className="project-card">
         <img src="/WorkdayScheduler.jpg" alt="Project 3" />
         <h2>Workday Scheduler</h2>
-        <p>Workday Scheduler allows you to note, and schedule out times during your day. It also uses Dayjs to allow for continous blockage of times throughout the day thatg have passed or are current.</p>
+        <p>
+Workday Scheduler allows you to easily note and schedule your daily tasks. Using Dayjs, it ensures continuous visualization of past and current time blocks for a seamless scheduling experience.</p>
         <div className="project-buttons">
           <a href="https://github.com/HenegarCodes/Workday-Scheduler" className="github-button">GitHub</a>
           <a href="https://henegarcodes.github.io/Workday-Scheduler/" className="deployed-button">Deployed</a>
@@ -39,7 +42,7 @@ const Projects = () => {
       <div className="project-card">
         <img src="/JungleMap.jpg" alt="Project 4" />
         <h2>Jungle Map</h2>
-        <p>Jungle Map is a NBA statistic app allowing users to get stats instantly for their favorite teams accompanied by a random gif of basketball on each click.</p>
+        <p>Jungle Map stands as your go-to NBA statistics app, delivering instant stats for your favorite teams. Enjoy the added flair of a random basketball gif with every click.</p>
         <div className="project-buttons">
           <a href="https://github.com/HenegarCodes/jungle-map-api" className="github-button">GitHub</a>
           <a href="https://bkness.github.io/jungle-map-api/" className="deployed-button">Deployed</a>
@@ -49,7 +52,7 @@ const Projects = () => {
       <div className="project-card">
         <img src="/WeatherApp.jpg" alt="Project 5" />
         <h2>5 Day Weather App</h2>
-        <p>The weather app allows users to instantly see their selective city of choices weather via 5 day report using the Open Weather API.</p>
+        <p>The Weather App offers users an immediate glimpse into the weather of their chosen city through a 5-day forecast using the Open Weather API.</p>
         <div className="project-buttons">
           <a href="https://github.com/HenegarCodes/5-Day-Weather-App" className="github-button">GitHub</a>
           <a href="https://henegarcodes.github.io/5-Day-Weather-App/" className="deployed-button">Deployed</a>
@@ -59,7 +62,7 @@ const Projects = () => {
       <div className="project-card">
         <img src="/RPS.jpg" alt="Project 6" />
         <h2>Rock, Paper, Scizzors</h2>
-        <p>Rock, Paper, Scizzors is a javascript application that keeps score of the classic game between the user and computer. It uses a random picker generator to pick the computers choice.</p>
+        <p>Rock, Paper, Scissors scores the classic game between the user and computer, adding an element of surprise with a random picker.</p>
         <div className="project-buttons">
           <a href="https://github.com/HenegarCodes/RockPaperScissors" className="github-button">GitHub</a>
           <a href="https://henegarcodes.github.io/RockPaperScissors/" className="deployed-button">Deployed</a>
